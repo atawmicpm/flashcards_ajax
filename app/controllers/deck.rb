f67112c30e03d3ef2c49c8a1e03b7bb7deccc310
Paulette Luftig <pyluftig@gmail.com>
2013-06-09 19:55:50 -0700

    updading decks

--- app/controllers/deck.rb
+++ app/controllers/deck.rb
@@ -3,10 +3,7 @@ get '/deck/create' do
 end
 
 post '/deck/create' do
-
   deck = Deck.create(topic: params[:topic])
-
-
 
   params[:questions].each do |question|
     deck.cards.create(question)
@@ -21,3 +18,40 @@ get '/deck/:deck_id' do |deck_id|
 
   erb :deck
 end
+
+get '/update/deck/:deck_id' do |id|
+	@deck = Deck.find(id)
+	@cards = Card.where('deck_id = ?', id)
+	erb :update_deck
+end
+
+post '/update/deck/:deck_id' do |id|
+	p "HERE ARE SOME PARAMS__ #{params}"
+	deck = Deck.find(id)
+
+	deck.update_attributes(topic: params[:topic])
+
+	# params[:questions].each do |question|
+ #    deck.cards.each do |card|
+    	
+ #    	card.update_attributes(question)
+ #    end
+ #  	end
+  # end
+
+	redirect "/profile/#{current_user.id}"
+end
+
+get '/delete/deck/:id' do |id|
+	Deck.destroy(id)
+	redirect "/profile/#{current_user.id}"
+end
+
+get '/delete/card/:id' do |id|
+	Card.destroy(id)
+	redirect "/profile/#{current_user.id}"
+end
+
+
+
+
